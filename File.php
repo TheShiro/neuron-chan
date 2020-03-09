1a97a6dfcd03380fb74d073da2e34438bf9c5727
TheShiro <smirnovsart@gmail.com>
2020-03-09 15:11:46 +0500

    Created class for work with file

---
+++ File.php
@@ -0,0 +1,18 @@
+<?php 
+
+class File 
+{
+
+	private static $path = "../networks/";
+
+	public static function get(string $filename) : string
+	{
+		return file_get_contents(self::$path . $filename);
+	}
+
+	public static function set(string $filename, string $data) : void
+	{
+		file_put_contents(self::$path . $filename, $data);
+	}
+
+}
